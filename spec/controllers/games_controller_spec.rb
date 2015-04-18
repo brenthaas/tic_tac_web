47723e5468f4273a86641a2640fefe17c4a01ee6
Brent Haas <brenthaas@gmail.com>
2015-04-18 13:57:00 -0700

    changed to using partial

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe GamesController do
+  let(:game) { Game.create }
+
   describe '#create' do
     it "creates a new Game" do
       expect { post :create }.to change { Game.count }.by(1)
@@ -13,8 +15,6 @@ describe GamesController do
   end
 
   describe "#show" do
-    let(:game) { Game.create }
-
     it "assigns the requested game" do
       get :show, id: game.id
       expect(assigns(:board)).to eq(game.board)
